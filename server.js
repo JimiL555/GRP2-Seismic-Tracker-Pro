ed69c397885694198f4beaa3c47c3ee21d9273f0
Jimi <liapis.jsl@gmail.com>
2024-09-18 19:54:27 -0400

    new start

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-//test
+const express = require('express');
+const session = require('express-session');
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Example session middleware
+app.use(session({
+    secret: 'super secret',
+    resave: false,
+    saveUninitialized: true,
+}));
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
